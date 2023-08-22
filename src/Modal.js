9a0dc4b647fb005118a463d547b7cfc4d173fb2e
Egor <ozuras53@gmail.com>
2023-08-22 21:44:35 +0300

    first commit

---
+++ src/Modal.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+// const Modal = ({ open, setOpen }) => (
+//   <div className={`overlay animated ${open ? "show" : ""}`}>
+//     <div className="modal">
+//       <svg
+//         onClick={() => setOpen(false)}
+//         height="200"
+//         viewBox="0 0 200 200"
+//         width="200"
+//       >
+//         <title />
+//         <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+//       </svg>
+//       <img
+//         alt="wooou"
+//         src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif"
+//       />
+//     </div>
+//   </div>
+// );
+
+function Modal({open, setOpen}) {
+    return (
+        <div className={`overlay animated ${open ? "show" : ""}`}>
+    <div className="modal">
+      <svg
+        onClick={() => setOpen(false)}
+        height="200"
+        viewBox="0 0 200 200"
+        width="200"
+      >
+        <title />
+        <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+      </svg>
+      <img
+        alt="wooou"
+        src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif"
+      />
+    </div>
+  </div>
+    )
+}
+
+export default Modal;
